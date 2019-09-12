c40587979c3c77ca9bc2e5abdf59cd5cc05bf3d8
taufiqgoe <54705540+taufiqgoe@users.noreply.github.com>
2019-09-12 21:22:07 +0800

    Ini adalah Tugas Kedua

--- H071191008.java
+++ H071191008.java
@@ -8,3 +8,16 @@ public static void main(String[] args){
     hasil = (float)waktu * kecepatan / 14;
     System.out.printf("Bensin Yang Digunakan : %.3f ",hasil);
 }
+
+public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        int detik,menit,jam;
+
+        detik = input.nextInt();
+        menit = detik/60;
+        jam = menit/60;
+
+        menit %= 60;
+        detik %= 60;
+        System.out.printf("%02d:%02d:%02d\n",jam,menit,detik);
+}
