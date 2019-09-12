0522cc61d6b433edfda33aff27768873b287a7b2
PandaNuisance <azizarsyi@gmail.com>
2019-09-13 06:44:55 +0800

    Kode ini mengkonversi waktu

---
+++ H071191003/Praktikum_1/Praktikum1No2.java
@@ -0,0 +1,16 @@
+/*Arsyi Syarief Aziz
+  NIM : H071191003
+*/
+import java.util.Scanner;
+class Praktikum1No2{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Input detik...");
+    int s = input.nextInt();
+    int m = s/60;
+    s %= 60;
+    int h = m/60;
+    m %=60;
+    System.out.printf("%02d:%02d:%02d \n\n",h,m,s);
+  }
+}
