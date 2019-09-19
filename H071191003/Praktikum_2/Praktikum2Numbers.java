260458675ab7d345b81f72c1d7a1a4c82c81ebe8
PandaNuisance <azizarsyi@gmail.com>
2019-09-19 23:40:21 +0800

    Converted loop into individual inputs

--- H071191003/Praktikum_2/Praktikum2Numbers.java
+++ H071191003/Praktikum_2/Praktikum2Numbers.java
@@ -8,26 +8,55 @@ class Praktikum2Numbers{
     int negative=0;
     System.out.println("Input 5 Integer!");
     try{
-    for(int i=0;i<5;i++){
-      int value = input.nextInt();
-      if(value%2==0){
-        even++;
-        if (value>0){
-          positive++;
-        }else if (value<0){
-          negative++;
-        }
-      }else{
-        odd ++;
-        if (value>0){
-          positive++;
-        }else if (value<0){
-          negative++;
-        }
-      }
+    int input1 = input.nextInt();
+    int input2 = input.nextInt();
+    int input3 = input.nextInt();
+    int input4 = input.nextInt();
+    int input5 = input.nextInt();
+    if (input1 % 2 ==0){
+      odd++;
     }
-    System.out.printf("%d Angka Genap\n",even);
-    System.out.printf("%d Angka Ganjil\n",odd);
+    if (input1>0){
+      positive++;
+    }else if(input1<0){
+      negative++;
+    }
+    if (input2 % 2 ==0){
+      even++;
+    }
+    if (input2>0){
+      positive++;
+    }else if(input2<0){
+      negative++;
+    }
+    if (input3 % 2 ==0){
+      even++;
+    }
+    if (input3>0){
+      positive++;
+    }else if(input3<0){
+      negative++;
+    }
+    if (input4 % 2 ==0){
+      even++;
+    }
+    if (input4>0){
+      positive++;
+    }else if(input4<0){
+      negative++;
+    }
+    if (input5 % 2 ==0){
+      even++;
+    }
+    odd = 5-even;
+    if (input5>0){
+      positive++;
+    }else if(input5<0){
+      negative++;
+    }
+    odd = 5-even;
+    System.out.printf("%d Angka even\n",even);
+    System.out.printf("%d Angka odd\n",odd);
     System.out.printf("%d Angka Positif\n",positive);
     System.out.printf("%d Angka Negatif\n",negative);
     }catch(Exception e){
