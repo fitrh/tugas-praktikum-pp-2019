bd269502f0f074a0f505f7c1fdbac7545f65c1e3
PandaNuisance <azizarsyi@gmail.com>
2019-09-19 23:51:23 +0800

    Fixed odd error

--- H071191003/Praktikum_2/Praktikum2Numbers.java
+++ H071191003/Praktikum_2/Praktikum2Numbers.java
@@ -13,14 +13,16 @@ class Praktikum2Numbers{
     int input3 = input.nextInt();
     int input4 = input.nextInt();
     int input5 = input.nextInt();
+
     if (input1 % 2 ==0){
-      odd++;
+      even++;
     }
     if (input1>0){
       positive++;
     }else if(input1<0){
       negative++;
     }
+
     if (input2 % 2 ==0){
       even++;
     }
@@ -29,6 +31,7 @@ class Praktikum2Numbers{
     }else if(input2<0){
       negative++;
     }
+
     if (input3 % 2 ==0){
       even++;
     }
@@ -37,6 +40,7 @@ class Praktikum2Numbers{
     }else if(input3<0){
       negative++;
     }
+
     if (input4 % 2 ==0){
       even++;
     }
@@ -45,18 +49,19 @@ class Praktikum2Numbers{
     }else if(input4<0){
       negative++;
     }
+
     if (input5 % 2 ==0){
       even++;
     }
-    odd = 5-even;
     if (input5>0){
       positive++;
     }else if(input5<0){
       negative++;
     }
+
     odd = 5-even;
-    System.out.printf("%d Angka even\n",even);
-    System.out.printf("%d Angka odd\n",odd);
+    System.out.printf("%d Angka genap\n",even);
+    System.out.printf("%d Angka ganjil\n",odd);
     System.out.printf("%d Angka Positif\n",positive);
     System.out.printf("%d Angka Negatif\n",negative);
     }catch(Exception e){
