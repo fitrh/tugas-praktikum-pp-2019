98ea3142c4430a0c809dfc6b36496c45a9fb5f7d
PandaNuisance <azizarsyi@gmail.com>
2019-11-09 11:36:21 +0800

    Fixed Student Data

--- H071191003/Praktikum_8/StudentData.java
+++ H071191003/Praktikum_8/StudentData.java
@@ -5,43 +5,45 @@ import java.util.Iterator;
 class CopyText {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Input name of source file (.txt): ");
-        String source = input.next();
         System.out.print("Save as : ");
-        String output = input.next();
-        FileReader fileR = null;
+        String source = input.next();
         FileWriter fileW = null;
-        BufferedReader in = null;
         BufferedWriter out = null;
         ArrayList<String> dataList = new ArrayList<>();
-        int longestLineLen = 0;
         try {
-            fileR = new FileReader(String.format("%s.txt", source));
-            fileW = new FileWriter(String.format("%s.txt", output));
-            in = new BufferedReader(fileR);
+            fileW = new FileWriter(String.format("%s.txt", source));
             out = new BufferedWriter(fileW);
-            input.close();
             String data;
-            int nameLen =4;
+            System.out.print("Input number of inputs : ");
+            int n = input.nextInt();
+            input.nextLine();
+            int iterate=0;
+            int nameLen = 4;
             int NIMLen = 3;
             int batchLen = 5;
-            int iterate=0;
-            while ((data = in.readLine()) != null) {
+            String[] dataType = {"Name : ","NIM : ","Batch : "};
+            while(iterate !=n*3){
+                if(iterate%3==0){
+                    System.out.printf("####Person %d####\n",((iterate/3)+1));
+                }
+                System.out.print(dataType[iterate%3]);
+                 data = input.nextLine();
                 dataList.add(data);
-                if (iterate%3==0 && data.length() > nameLen) {
+                if (iterate % 3 == 0 && data.length() > nameLen) {
                     nameLen = data.length();
-                }else if(iterate%3==1 && data.length()>NIMLen){
+                } else if (iterate % 3 == 1 && data.length() > NIMLen) {
                     NIMLen = data.length();
-                }else if (iterate%3==2 && data.length()>batchLen){
+                } else if (iterate % 3 == 2 && data.length() > batchLen) {
                     batchLen = data.length();
                 }
                 iterate++;
             }        
+            input.close();
             Iterator<String> iterator = dataList.iterator();
             int sizeI=(int)Math.ceil((double)dataList.size()/3)+4;    
             int sizeJ;            
             for(int i=0;i<(sizeI=(sizeI==4?3:sizeI));i++){
-                for(int j=0;j<(sizeJ = (nameLen + NIMLen + batchLen>16? nameLen + NIMLen + batchLen+4:16));j++){
+                for(int j=0;j<(sizeJ = nameLen + NIMLen + batchLen+4);j++){
                     if (j == 0 || j == nameLen + 1 || j == NIMLen + 2 + nameLen || j == batchLen + 3 + NIMLen + nameLen) {
                         if(i==0 || i==2 || i==sizeI-1){
                             out.write("+");
@@ -81,13 +83,6 @@ class CopyText {
                     }
                 }
                 out.write("\n");
-                
-            }
-            if (in != null) {
-                in.close();
-            }
-            if (in != null) {
-                out.close();
             }
             System.out.println("Success!");
 
@@ -95,12 +90,7 @@ class CopyText {
             System.out.println("Fail!");
         } finally {
             try {
-                if (in != null) {
-                    in.close();
-                }
-                if (in != null) {
-                    out.close();
-                }
+                out.close();
             } catch (IOException e) {
                 System.out.println("Fail!");
             }
